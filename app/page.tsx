322bc4b601233f7c828c36dcc9000cee7c952b04
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2024-04-18 20:20:05 +0800

    Modify page add todo

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import Todo from './todo'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Todo>
-
-      </Todo>
+      <Todo />
     </main>
   );
 }

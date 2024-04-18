fda802b9abefac94f6d8cb5a3bebab6e1b6e7191
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2024-04-18 20:16:14 +0800

    Add tasks complete button

--- app/todo.tsx
+++ app/todo.tsx
@@ -1,22 +1,52 @@
+"use client"
 import { useState } from 'react';
 
 interface FnTask {
-    isComplete: Boolean;
-    title: String;
+    isComplete: boolean;
+    title: string;
 }
 
 export default function Todo() {
+    const [taskTitle, setTaskTitle] = useState<string>("");
     const [tasks, setTasks] = useState<FnTask[]>([]);
 
-    function addTask(taskTitle: string) {
-        const newTask: FnTask = {
-            isComplete: false,
-            title: taskTitle
-        };
-        setTasks([...tasks, newTask]);
+    const addTask = (): void => {
+        if (taskTitle.trim() !== "") {
+            const newTask: FnTask = {
+                isComplete: false,
+                title: taskTitle
+            };
+            setTasks([...tasks, newTask]);
+            setTaskTitle(""); // 清空输入字段
+        }
+    }
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setTaskTitle(e.target.value);
+    }
+
+    const handleClick = (index: number): void => {
+        const newTasks = [...tasks];
+        newTasks[index].isComplete = !(tasks[index].isComplete);
+        setTasks(newTasks);
     }
 
     return (
-        <div>{tasks.map((task) => <>{task}</>)}</div>
+        <>
+            <div>
+                <div>{tasks.map((task, index) =>
+                    <div key={index}>
+                        <button onClick={() => handleClick(index)}>
+                            {task.isComplete ? "✔" : "❌"}
+                        </button>
+                        {task.title}
+                    </div>)}
+                </div>
+                <div>
+                    <input type="string" value={taskTitle} onChange={handleTitleChange} />
+                    <button onClick={addTask} value={taskTitle}>Add</button>
+                </div>
+            </div>
+        </>
     )
 }

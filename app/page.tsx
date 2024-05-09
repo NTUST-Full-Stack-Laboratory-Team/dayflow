d8744d4237fa509d6c08c6b9d85afb4dd4cfad76
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2024-05-10 01:35:49 +0800

    Finish basic page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Todo from './todo'
+import Image from "next/image";
+import DayFlowSVG from "../public/DayFlow.svg";
 import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons'
 
 export default function Home() {
   return (
     <div className='flex min-h-screen min-w-full flex-col items-center'>
-      <h1>DayFlow</h1>
-      <div className="flex min-h-screen min-w-full flex-row items-center justify-between">
+      <Image src={DayFlowSVG}
+        width={200}
+        height={200}
+        alt="DayFlow" />
+      <div className="flex min-h-screen min-w-full flex-row items-center justify-around">
         <div>
           <button>{<LeftCircleOutlined style={{ fontSize: '200%' }} />}</button>
         </div>

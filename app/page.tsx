5833a3f16a5a78a7c3c5612f213f5dd11520d327
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2024-05-10 13:30:17 +0800

    Add schedule position

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,83 @@
 import Todo from './todo'
 import Image from "next/image";
 import DayFlowSVG from "../public/DayFlow.svg";
-import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons'
+import { LeftCircleOutlined, RightCircleOutlined, SmileOutlined } from '@ant-design/icons'
+import { Timeline } from 'antd';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen min-w-full flex-col items-center'>
-      <Image src={DayFlowSVG}
-        width={200}
-        height={200}
-        alt="DayFlow" />
-      <div className="flex min-h-screen min-w-full flex-row items-center justify-around">
-        <div>
-          <button>{<LeftCircleOutlined style={{ fontSize: '200%' }} />}</button>
-        </div>
+    <div className="flex min-h-screen min-w-full flex-row items-center justify-around">
+      <button>{<LeftCircleOutlined style={{ fontSize: '200%' }} />}</button>
+      <div className='flex min-h-screen min-w-90 flex-col items-center'>
+        <Image src={DayFlowSVG}
+          width={200}
+          height={200}
+          alt="DayFlow" />
         <div className='flex flex-row items-center justify-start'>
-          <Todo />&nbsp;
-          <div>qwq</div>
+          <div className="flex min-h-screen min-w-80 items-center justify-center">
+            <Todo />&nbsp;
+          </div>
+          <div className="flex min-h-screen min-w-80 items-center justify-center">
+            <Timeline
+              items={[
+                {
+                  color: 'green',
+                  children: 'Create a services site 2015-09-01',
+                },
+                {
+                  color: 'green',
+                  children: 'Create a services site 2015-09-01',
+                },
+                {
+                  color: 'red',
+                  children: (
+                    <>
+                      <p>Solve initial network problems 1</p>
+                      <p>Solve initial network problems 2</p>
+                      <p>Solve initial network problems 3 2015-09-01</p>
+                    </>
+                  ),
+                },
+                {
+                  children: (
+                    <>
+                      <p>Technical testing 1</p>
+                      <p>Technical testing 2</p>
+                      <p>Technical testing 3 2015-09-01</p>
+                    </>
+                  ),
+                },
+                {
+                  color: 'gray',
+                  children: (
+                    <>
+                      <p>Technical testing 1</p>
+                      <p>Technical testing 2</p>
+                      <p>Technical testing 3 2015-09-01</p>
+                    </>
+                  ),
+                },
+                {
+                  color: 'gray',
+                  children: (
+                    <>
+                      <p>Technical testing 1</p>
+                      <p>Technical testing 2</p>
+                      <p>Technical testing 3 2015-09-01</p>
+                    </>
+                  ),
+                },
+                {
+                  color: '#00CCFF',
+                  dot: <SmileOutlined />,
+                  children: <p>Custom color testing</p>,
+                },
+              ]}
+            />
+          </div>
         </div>
-        <button>{<RightCircleOutlined style={{ fontSize: '200%' }} />}</button>
       </div>
+      <button>{<RightCircleOutlined style={{ fontSize: '200%' }} />}</button>
     </div>
   );
 }

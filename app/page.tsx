c6424b4a3523362de8560f7c8aabf48f26ce99d5
AllenTing <allen0523win@gmail.com>
2024-05-11 20:50:43 +0800

    Add schedule component into page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Todo from './todo'
 import Memo from "./memo"
 import Day from "./day"
 import Completeness from './Completeness'
+import Schedule from './schedule'
 import { Divider } from "antd";
 import Image from "next/image";
 import DayFlowSVG from "../public/DayFlow.svg";
@@ -37,68 +38,13 @@ export default function Home() {
           </div>
         </div>
         {/* <Divider type="vertical" /> */}
-        <div className='mt-6 w-3/6'>
-          <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Schedule</div>
-          <div className='scheduleContainer'>
-            {
-              <Timeline
-                items={[
-                  {
-                    color: 'green',
-                    children: 'Create a services site 2015-09-01',
-                  },
-                  {
-                    color: 'green',
-                    children: 'Create a services site 2015-09-01',
-                  },
-                  {
-                    color: 'red',
-                    children: (
-                      <>
-                        <p>Solve initial network problems 1</p>
-                        <p>Solve initial network problems 2</p>
-                        <p>Solve initial network problems 3 2015-09-01</p>
-                      </>
-                    ),
-                  },
-                  {
-                    children: (
-                      <>
-                        <p>Technical testing 1</p>
-                        <p>Technical testing 2</p>
-                        <p>Technical testing 3 2015-09-01</p>
-                      </>
-                    ),
-                  },
-                  {
-                    color: 'gray',
-                    children: (
-                      <>
-                        <p>Technical testing 1</p>
-                        <p>Technical testing 2</p>
-                        <p>Technical testing 3 2015-09-01</p>
-                      </>
-                    ),
-                  },
-                  {
-                    color: 'gray',
-                    children: (
-                      <>
-                        <p>Technical testing 1</p>
-                        <p>Technical testing 2</p>
-                        <p>Technical testing 3 2015-09-01</p>
-                      </>
-                    ),
-                  },
-                  {
-                    color: '#00CCFF',
-                    dot: <SmileOutlined />,
-                    children: <p>Custom color testing</p>,
-                  },
-                ]} />
-            }
+        <div className='ml-4 mt-6 w-3/6'>
+          <div className='w-full h-4/5'>
+            <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Schedule</div>
+            <Schedule />
           </div>
-          <div>
+          <div className='w-full h-1/5'>
+            <Divider />
             <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Completeness</div>
             <Completeness />
           </div>

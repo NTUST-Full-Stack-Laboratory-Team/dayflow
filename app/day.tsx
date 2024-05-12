e3a462d8228927b7f9971333cbb90ca36f259dcf
AllenTing <allen0523win@gmail.com>
2024-05-12 16:04:15 +0800

    Add date css

--- app/day.tsx
+++ app/day.tsx
@@ -32,15 +32,18 @@ export default function Day() {
     const dayItems = generateMenuItems(1, 31, handleDayClick);
 
     return (
-        <div className="flex flex-row items-center">
-            <Dropdown menu={{ items: monthItems }} placement="bottomLeft" trigger={['click']}>
-                <Button className="font-jacques-Francois text-xl" type="text">{month}</Button>
-            </Dropdown>
-            <div className="font-jacques-Francois text-2xl">/</div>
-            <Dropdown menu={{ items: dayItems }} placement="bottomLeft" trigger={['click']}>
-                <Button className="font-jacques-Francois text-xl" type="text">{day}</Button>
-            </Dropdown>
-            <div className="font-jacques-Francois text-xl">{weekday}</div>
+        <div className="flex flex-col items-start">
+            <div className="flex items-center font-jacques-Francois text-2xl text-slate-500 bg-[#D9D9D9] w-32 h-10 pl-2">{weekday}</div>
+            <br />
+            <div className="flex flex-row items-center">
+                <Dropdown menu={{ items: monthItems }} placement="bottomLeft" trigger={['click']}>
+                    <Button className="font-jacques-Francois text-xl" type="text">{month}</Button>
+                </Dropdown>
+                <div className="font-jacques-Francois text-2xl">/</div>
+                <Dropdown menu={{ items: dayItems }} placement="bottomLeft" trigger={['click']}>
+                    <Button className="font-jacques-Francois text-xl" type="text">{day}</Button>
+                </Dropdown>
+            </div>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <div className='z-10 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] flex w-[45vw] h-[90vh] flex-row justify-around'>
         <div className='mt-6 flex h-screen w-2/5 flex-col items-start'>
           <div className='ml-4 w-full h-auto'>
-            <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Date</div>
+            {/* <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Date</div> */}
             <Day />
           </div>
           <div className='ml-4 w-full h-1/3'>

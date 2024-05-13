e83f36892df7f8d6c239e7ab1d2a07c9eda90c48
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2024-05-13 14:47:38 +0800

    Change date chosen way

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import DayFlowSVG from "../public/DayFlow.svg";
 export default function Home() {
   return (
     <div className='bg-[#FCFCFC] flex h-screen w-full flex-row items-center justify-center'>
-      <div className='relative left-10 z-0 hover:z-50 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] w-[20vw] h-[85vh]'>
+      <div className='overflow-scroll relative left-10 z-0 hover:z-50 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] w-[20vw] h-[85vh]'>
         <div className='group flex flex-col items-start h-full'>
           <span className='font-jacques-Francois ml-4 mt-6 text-[#CEA282] text-3xl w-full h-16'>Weekly Plan</span>
           <div className='ml-4 w-5/6 h-5/6 overflow-scroll'>
@@ -18,8 +18,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className='z-10 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] flex w-[45vw] h-[90vh] flex-row justify-around'>
-        <div className='mt-6 flex h-full w-2/5 flex-col items-start'>
+      <div className='overflow-scroll z-10 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] flex w-[45vw] h-[90vh] flex-row justify-around'>
+        <div className='mt-6 flex h-[85vh] w-2/5 flex-col items-start'>
           <div className='ml-4 w-full h-[10%]'>
             {/* <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Date</div> */}
             <Day />
@@ -34,20 +34,20 @@ export default function Home() {
           <div className='ml-4 w-full h-[45%]'>
             <Divider />
             <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Memo</div>
-            <div className='h-[70%]'>
+            <div className='h-[80%]'>
               <Memo />
             </div>
           </div>
         </div>
         {/* <Divider type="vertical" /> */}
-        <div className='ml-4 mt-6 w-3/6'>
+        <div className='ml-4 mt-6 w-3/6 h-[85vh]'>
           <div className='group w-full h-4/5'>
             <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Schedule</div>
             <div className='pt-2 h-[90%] overflow-scroll'>
               <Schedule />
             </div>
           </div>
-          <div className='w-full h-1/5'>
+          <div className='w-full h-1/6'>
             <Divider />
             <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Completeness</div>
             <Completeness />

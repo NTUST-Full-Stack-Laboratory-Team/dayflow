119d925580ddce99aca996b4d8842f000e69f6a7
AllenTing <allen0523win@gmail.com>
2024-05-14 11:11:33 +0800

    fix-scroll-bar

--- app/page.tsx
+++ app/page.tsx
@@ -29,16 +29,16 @@ export default function Home() {
       <div className='flex h-screen w-full flex-row items-center justify-center' style={{ backgroundImage: `url('${bg.src}')`, backgroundRepeat: "no-repeat", backgroundSize: "100%" }}>
         <div className='flex flex-row items-center justify-center' style={{ transform: `translate3d(${offset}px, 0, 0)` }}>
 
-          <div className='weekly-plan-card  hover:transform-hover overflow-scroll z-0 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] w-[20vw] h-[85vh]' ref={weeklyPlanCardRef}>
+          <div className='weekly-plan-card  hover:transform-hover overflow-auto z-0 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] w-[20vw] h-[85vh]' ref={weeklyPlanCardRef}>
             <div className='group flex flex-col items-start h-full'>
               <span className='font-jacques-Francois ml-4 mt-6 text-[#CEA282] text-3xl w-full h-16'>Weekly Plan</span>
-              <div className='ml-4 w-5/6 h-5/6 overflow-scroll'>
+              <div className='ml-4 w-5/6 h-5/6 overflow-auto'>
                 <Todo />
               </div>
             </div>
           </div>
 
-          <div className='overflow-scroll z-0 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] flex w-[45vw] h-[90vh] flex-row justify-around' >
+          <div className='overflow-auto z-0 bg-[#FCFCFC] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] flex w-[45vw] h-[90vh] flex-row justify-around' >
             <div className='mt-6 flex h-[85vh] w-2/5 flex-col items-start'>
               <div className='ml-4 w-full h-[10%]'>
                 {/* <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Date</div> */}
@@ -47,7 +47,7 @@ export default function Home() {
               <div className='ml-4 w-full h-[40%]'>
                 <Divider />
                 <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Day Todo</div>
-                <div className='group w-full h-4/6 overflow-scroll'>
+                <div className='group w-full h-4/6 overflow-auto'>
                   <Todo />
                 </div>
               </div>
@@ -63,7 +63,7 @@ export default function Home() {
             <div className='ml-4 mt-6 w-3/6 h-[85vh]'>
               <div className='group w-full h-4/5'>
                 <div className='font-jacques-Francois text-2xl text-[#CEA282] w-full h-14'>Schedule</div>
-                <div className='pt-2 h-[90%] overflow-scroll'>
+                <div className='pt-2 h-[90%] overflow-auto'>
                   <Schedule />
                 </div>
               </div>
